25107d21bffb566b5b42e2b6a808df7ffa0a47e0
Michael Sisay <ashenafimichael25@gmail.com>
2020-05-05 22:23:02 -0500

    project intialization

---
+++ src/main/java/com/webapps/betsale/BetsaleApplication.java
@@ -0,0 +1,13 @@
+package com.webapps.betsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetsaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webapps/betsale/BetsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapps.betsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
